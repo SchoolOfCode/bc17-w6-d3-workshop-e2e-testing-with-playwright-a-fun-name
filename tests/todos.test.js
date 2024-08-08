9366b375262f9a8660a587001b07e9769335efb0
kieranp-p6 <kiip96@icloud.com>
2024-08-08 12:12:51 +0100

    completed task 5

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,5 +1,11 @@
-import { test } from "@playwright/test";
+import {expect, test} from "@playwright/test";
 
-test("Practice test", async ({ page }) => {
+test("Testing for input field", async ({ page }) => {
+    // goes to page at this url
     await page.goto('http://localhost:3000');
+    // trying to find input on the page
+    const locator = page.getByRole('textbox');
+    await locator.fill("Dummy Data");
+
+  await expect(locator).toHaveValue("Dummy Data");
 });
